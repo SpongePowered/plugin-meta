6a3bbfd13449199d0f4cf1f095e692fcd1f8072b
Minecrell <dev@minecrell.net>
2016-11-16 22:01:02 +0100

    Add Javadocs
    - Remove SpongeExtension

--- src/main/java/org/spongepowered/plugin/meta/McModInfo.java
+++ src/main/java/org/spongepowered/plugin/meta/McModInfo.java
@@ -28,6 +28,7 @@ package org.spongepowered.plugin.meta;
 import static java.util.Arrays.asList;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonIOException;
 import com.google.gson.stream.JsonReader;
@@ -43,19 +44,31 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.io.Writer;
+import java.lang.reflect.Type;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+/**
+ * Represents a serializer for {@link PluginMetadata} for the
+ * {@code mcmod.info} file format.
+ */
 public final class McModInfo {
 
+    /**
+     * The file name the metadata is usually saved in.
+     */
     public static final String STANDARD_FILENAME = "mcmod.info";
 
     private static final Charset CHARSET = StandardCharsets.UTF_8;
     private static final String INDENT = "    ";
 
+    /**
+     * The default serializer that converts all additional properties into
+     * standard Java types.
+     */
     public static final McModInfo DEFAULT = new McModInfo(ModMetadataCollectionAdapter.DEFAULT);
 
     private final ModMetadataCollectionAdapter adapter;
@@ -64,6 +77,13 @@ public final class McModInfo {
         this.adapter = adapter;
     }
 
+    /**
+     * Deserializes the specified JSON string into a {@link List} of
+     * {@link PluginMetadata}.
+     *
+     * @param json The JSON string
+     * @return The deserialized metadata list
+     */
     public List<PluginMetadata> fromJson(String json) {
         try {
             return this.adapter.fromJson(json);
@@ -72,28 +92,73 @@ public final class McModInfo {
         }
     }
 
+    /**
+     * Reads a {@link List} of {@link PluginMetadata} from the file represented
+     * by the specified {@link Path}.
+     *
+     * @param path The path to the file
+     * @return The deserialized metadata list
+     * @throws IOException If an error occurs while reading
+     */
     public List<PluginMetadata> read(Path path) throws IOException {
         try (BufferedReader reader = Files.newBufferedReader(path, CHARSET)) {
             return read(reader);
         }
     }
 
+    /**
+     * Reads a {@link List} of {@link PluginMetadata} from the given
+     * {@link InputStream}.
+     *
+     * @param in The input stream
+     * @return The deserialized metadata list
+     * @throws IOException If an error occurs while reading
+     */
     public List<PluginMetadata> read(InputStream in) throws IOException {
         return this.adapter.fromJson(new BufferedReader(new InputStreamReader(in, CHARSET)));
     }
 
+    /**
+     * Reads a {@link List} of {@link PluginMetadata} from the given
+     * {@link Reader}.
+     *
+     * @param reader The reader
+     * @return The deserialized metadata list
+     * @throws IOException If an error occurs while reading
+     */
     public List<PluginMetadata> read(Reader reader) throws IOException {
         return this.adapter.fromJson(reader);
     }
 
+    /**
+     * Reads a {@link List} of {@link PluginMetadata} from the given
+     * {@link JsonReader}.
+     *
+     * @param reader The JSON reader
+     * @return The deserialized metadata list
+     * @throws IOException If an error occurs while reading
+     */
     public List<PluginMetadata> read(JsonReader reader) throws IOException {
         return this.adapter.read(reader);
     }
 
+    /**
+     * Serializes the specified {@link PluginMetadata} to a JSON string.
+     *
+     * @param meta The plugin metadata to serialize
+     * @return The serialized JSON string
+     */
     public String toJson(PluginMetadata... meta) {
         return toJson(asList(meta));
     }
 
+    /**
+     * Serializes the specified {@link List} of {@link PluginMetadata} to a
+     * JSON string.
+     *
+     * @param meta The plugin metadata to serialize
+     * @return The serialized JSON string
+     */
     public String toJson(List<PluginMetadata> meta) {
         StringWriter writer = new StringWriter();
         try {
@@ -104,20 +169,51 @@ public final class McModInfo {
         return writer.toString();
     }
 
+    /**
+     * Writes the specified {@link PluginMetadata} to the file represented by
+     * the {@link Path}.
+     *
+     * @param path The path to the file to write to
+     * @param meta The plugin metadata to serialize
+     * @throws IOException If an error occurs while writing
+     */
     public void write(Path path, PluginMetadata... meta) throws IOException {
         write(path, asList(meta));
     }
 
+    /**
+     * Writes the specified {@link List} of {@link PluginMetadata} to the file
+     * represented by the {@link Path}.
+     *
+     * @param path The path to the file to write to
+     * @param meta The plugin metadata to serialize
+     * @throws IOException If an error occurs while writing
+     */
     public void write(Path path, List<PluginMetadata> meta) throws IOException {
         try (BufferedWriter writer = Files.newBufferedWriter(path, CHARSET)) {
             write(writer, meta);
         }
     }
 
+    /**
+     * Writes the specified {@link PluginMetadata} to the {@link Writer}.
+     *
+     * @param writer The writer
+     * @param meta The plugin metadata to serialize
+     * @throws IOException If an error occurs while writing
+     */
     public void write(Writer writer, PluginMetadata... meta) throws IOException {
         write(writer, asList(meta));
     }
 
+    /**
+     * Writes the specified {@link List} of {@link PluginMetadata} to the
+     * {@link Writer}.
+     *
+     * @param writer The writer
+     * @param meta The plugin metadata to serialize
+     * @throws IOException If an error occurs while writing
+     */
     public void write(Writer writer, List<PluginMetadata> meta) throws IOException {
         try (JsonWriter json = new JsonWriter(writer)) {
             json.setIndent(INDENT);
@@ -126,18 +222,45 @@ public final class McModInfo {
         }
     }
 
+    /**
+     * Writes the specified {@link PluginMetadata} to the {@link JsonWriter}.
+     *
+     * @param writer The JSON writer
+     * @param meta The plugin metadata to serialize
+     * @throws IOException If an error occurs while writing
+     */
     public void write(JsonWriter writer, PluginMetadata... meta) throws IOException {
         write(writer, asList(meta));
     }
 
+    /**
+     * Writes the specified {@link List} of {@link PluginMetadata} to the
+     * {@link JsonWriter}.
+     *
+     * @param writer The JSON writer
+     * @param meta The plugin metadata to serialize
+     * @throws IOException If an error occurs while writing
+     */
     public void write(JsonWriter writer, List<PluginMetadata> meta) throws IOException {
         this.adapter.write(writer, meta);
     }
 
+    /**
+     * Creates a new {@link Builder} that can be used to construct a
+     * {@link McModInfo} serializer that serializes specific extension keys
+     * to their Java object representation.
+     *
+     * @return A new builder
+     */
     public static Builder builder() {
         return new Builder();
     }
 
+    /**
+     * Represents a builder for {@link McModInfo} serializes that can be used
+     * to construct a serializer that serializes specific extension keys to
+     * their Java object representation.
+     */
     public static final class Builder {
 
         private final GsonBuilder gson = new GsonBuilder();
@@ -146,21 +269,54 @@ public final class McModInfo {
         private Builder() {
         }
 
+        /**
+         * Returns the {@link GsonBuilder} that will be used to build this
+         * serializer's {@link Gson} instance. Modify the builder if you
+         * need to set custom options to make your extension serializer
+         * work properly.
+         *
+         * @return The GSON builder
+         */
         public GsonBuilder gson() {
             return this.gson;
         }
 
+        /**
+         * Registers an extension with the given key and the specified
+         * extension class. By default, GSON will serializer the extension
+         * class using a representation of the public fields.
+         *
+         * @param key The key of the extension
+         * @param extensionClass The class to serialize the extension to
+         * @return This builder instance
+         */
         public Builder registerExtension(String key, Class<?> extensionClass) {
             this.extensions.put(key, extensionClass);
             return this;
         }
 
+        /**
+         * Registers an extension with the given key, extension class and a
+         * custom type adapter to use for serializing the extension class.
+         *
+         * @param key The key of the extension
+         * @param extensionClass The class to serialize the extension to
+         * @param typeAdapter The type adapter to use for serializing
+         * @return This builder instance
+         *
+         * @see GsonBuilder#registerTypeAdapter(Type, Object)
+         */
         public Builder registerExtension(String key, Class<?> extensionClass, Object typeAdapter) {
             registerExtension(key, extensionClass);
             this.gson.registerTypeAdapter(extensionClass, typeAdapter);
             return this;
         }
 
+        /**
+         * Builds the {@link McModInfo} serializer using the specified options.
+         *
+         * @return The built serializer
+         */
         public McModInfo build() {
             return new McModInfo(new ModMetadataCollectionAdapter(new ModMetadataAdapter(this.gson.create(), this.extensions.build())));
         }
--- src/main/java/org/spongepowered/plugin/meta/PluginMetadata.java
+++ src/main/java/org/spongepowered/plugin/meta/PluginMetadata.java
@@ -32,15 +32,28 @@ import static com.google.common.base.Strings.emptyToNull;
 import com.google.common.base.Objects;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 import javax.annotation.Nullable;
 
+/**
+ * Represents additional metadata for a specific version of a plugin.
+ */
 public final class PluginMetadata {
+
+    /**
+     * The pattern plugin IDs must match. Plugin IDs must be lower case, and
+     * start with an alphabetic character. It may only contain alphanumeric
+     * characters, dashes or underscores. It cannot be longer than
+     * 64 characters.
+     */
+    public static final Pattern ID_PATTERN = Pattern.compile("[a-z][a-z0-9-_]{0,63}");
 
     private String id;
     @Nullable private String name;
@@ -57,91 +70,162 @@ public final class PluginMetadata {
 
     private final Map<String, Object> extensions = new LinkedHashMap<>();
 
+    /**
+     * Constructs a new {@link PluginMetadata} with the specified plugin ID.
+     *
+     * @param id The plugin ID, must match {@link #ID_PATTERN}
+     */
     public PluginMetadata(String id) {
         setId(id);
     }
 
+    /**
+     * Returns the plugin ID that is represented by this {@link PluginMetadata}.
+     *
+     * @return The plugin ID
+     */
     public String getId() {
         return this.id;
     }
 
+    /**
+     * Sets the plugin ID that is represented by this {@link PluginMetadata}.
+     *
+     * @param id The plugin ID, must match {@link #ID_PATTERN}
+     */
     public void setId(String id) {
         checkNotNull(id, "id");
-        checkArgument(!id.isEmpty(), "id cannot be empty");
+        checkArgument(ID_PATTERN.matcher(id).matches(), "id must match pattern %s", ID_PATTERN);
         this.id = id;
     }
 
+    /**
+     * Returns the plugin name.
+     *
+     * @return The plugin name or {@code null} if unknown
+     */
     @Nullable
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Sets the plugin name.
+     *
+     * @param name The plugin name or {@code null} to reset
+     */
     public void setName(@Nullable String name) {
         this.name = emptyToNull(name);
     }
 
+    /**
+     * Returns the plugin version.
+     *
+     * @return The plugin version or {@code null} if unknown
+     */
     @Nullable
     public String getVersion() {
         return this.version;
     }
 
+    /**
+     * Sets the plugin version.
+     *
+     * @param version The plugin version or {@code null} to reset
+     */
     public void setVersion(@Nullable String version) {
         this.version = emptyToNull(version);
     }
 
+    /**
+     * Returns the plugin description.
+     *
+     * @return The plugin description or {@code null} if unknown
+     */
     @Nullable
     public String getDescription() {
         return this.description;
     }
 
+    /**
+     * Sets the plugin description.
+     *
+     * @param description The plugin description or {@code null} to reset
+     */
     public void setDescription(@Nullable String description) {
         this.description = emptyToNull(description);
     }
 
+    /**
+     * Returns a URL where additional information about a plugin may be found.
+     *
+     * @return The URL or {@code null} if unknown
+     */
     @Nullable
     public String getUrl() {
         return this.url;
     }
 
+    /**
+     * Sets a URL where additional information about a plugin may be found.
+     *
+     * @param url The URL or {@code null} to reset
+     */
     public void setUrl(@Nullable String url) {
         this.url = emptyToNull(url);
     }
 
+    /**
+     * Returns a mutable {@link List} of authors for this plugin.
+     *
+     * <p>The returned list can be used to remove an author from the
+     * metadata.</p>
+     *
+     * @return The list of authors, can be empty
+     */
     public List<String> getAuthors() {
         return this.authors;
     }
 
+    /**
+     * Adds an author to the list of authors for this plugin.
+     *
+     * @param author The author to add
+     */
     public void addAuthor(String author) {
         checkNotNull(author, "author");
-        if (author.isEmpty()) {
-            return;
-        }
-
+        checkArgument(!author.isEmpty(), "Author cannot be empty");
         this.authors.add(author);
     }
 
+    @Deprecated
     public Set<Dependency> getRequiredDependencies() {
         return this.dependencies;
     }
 
+    @Deprecated
     public Set<Dependency> getLoadBefore() {
         return this.loadBefore;
     }
 
+    @Deprecated
     public Set<Dependency> getLoadAfter() {
         return this.loadAfter;
     }
 
+    @Deprecated
     public void addRequiredDependency(Dependency dependency) {
         checkNotNull(dependency, "dependency");
         this.dependencies.add(dependency);
     }
 
+    @Deprecated
     public void loadBefore(Dependency dependency) {
         checkNotNull(dependency, "dependency");
         this.loadBefore.add(dependency);
     }
 
+    @Deprecated
     public void loadBefore(Dependency dependency, boolean required) {
         loadBefore(dependency);
         if (required) {
@@ -149,11 +233,13 @@ public final class PluginMetadata {
         }
     }
 
+    @Deprecated
     public void loadAfter(Dependency dependency) {
         checkNotNull(dependency, "dependency");
         this.loadAfter.add(dependency);
     }
 
+    @Deprecated
     public void loadAfter(Dependency dependency, boolean required) {
         loadAfter(dependency);
         if (required) {
@@ -161,18 +247,53 @@ public final class PluginMetadata {
         }
     }
 
+    /**
+     * Returns a map with additional properties for this {@link PluginMetadata}.
+     *
+     * <p>To serialize arbitrary objects, it may be necessary to register a
+     * custom serializer for the extension object.</p>
+     *
+     * @return An unmodifiable map with additional properties
+     */
     public Map<String, Object> getExtensions() {
-        return this.extensions;
+        return Collections.unmodifiableMap(this.extensions);
     }
 
+    /**
+     * Returns the value for a specific extension.
+     *
+     * <p><b>Note:</b> This method will not verify if this metadata contains a
+     * value with the specified type. A {@link ClassCastException} will be
+     * thrown if the value has a different type.</p>
+     *
+     * @param key The key of the extension
+     * @param <T> The type to cast the extension to
+     * @return The extension or {@code null} if not set
+     */
     @Nullable
     @SuppressWarnings("unchecked")
     public <T> T getExtension(String key) {
         return (T) this.extensions.get(key);
     }
 
+    /**
+     * Adds the specified extension to this {@link PluginMetadata}.
+     *
+     * @param key The key of the extension
+     * @param extension The extension
+     */
     public void setExtension(String key, Object extension) {
-       this.extensions.put(key, extension);
+        checkNotNull(extension, "extension");
+        this.extensions.put(key, extension);
+    }
+
+    /**
+     * Removes an extension from this {@link PluginMetadata}.
+     *
+     * @param key The key of the extension
+     */
+    public void removeExtension(String key) {
+        this.extensions.remove(key);
     }
 
     @Override
@@ -192,21 +313,70 @@ public final class PluginMetadata {
                 .toString();
     }
 
+    /**
+     * Represents a dependency on another plugin.
+     */
     public static final class Dependency {
 
         private final String id;
         @Nullable private final String version;
 
-        public Dependency(String id, String version) {
+        /**
+         * Constructs a new {@link Dependency} with the given plugin ID.
+         *
+         * @param id The plugin ID of the dependency
+         */
+        public Dependency(String id) {
+            this(id, null);
+        }
+
+        /**
+         * Constructs a new {@link Dependency} with the given plugin ID and
+         * version range.
+         *
+         * @param id The plugin ID of the dependency
+         * @param version The version range of the dependency or {@code null}
+         *
+         * @see #getVersion() Version range syntax
+         */
+        public Dependency(String id, @Nullable String version) {
             this.id = checkNotNull(id, "id");
             checkArgument(!id.isEmpty(), "id cannot be empty");
             this.version = emptyToNull(version);
         }
 
+        /**
+         * Returns the plugin ID of this {@link Dependency}.
+         *
+         * @return The plugin ID
+         */
         public String getId() {
             return this.id;
         }
 
+        /**
+         * Returns the version range this {@link Dependency} should match.
+         *
+         * <p>The version range should use the <b>Maven version range
+         * syntax</b>:</p>
+         *
+         * <table>
+         * <caption>Maven version range syntax</caption>
+         * <tr><th>Range</th><th>Meaning</th></tr>
+         * <tr><td>1.0</td><td>Any dependency version, 1.0 is recommended</td></tr>
+         * <tr><td>[1.0]</td><td>x == 1.0</td></tr>
+         * <tr><td>[1.0,)</td><td>x &gt;= 1.0</td></tr>
+         * <tr><td>(1.0,)</td><td>x &gt; 1.0</td></tr>
+         * <tr><td>(,1.0]</td><td>x &lt;= 1.0</td></tr>
+         * <tr><td>(,1.0)</td><td>x &lt; 1.0</td></tr>
+         * <tr><td>(1.0,2.0)</td><td>1.0 &lt; x &lt; 2.0</td></tr>
+         * <tr><td>[1.0,2.0]</td><td>1.0 &lt;= x &lt;= 2.0</td></tr>
+         * </table>
+         *
+         * @return The version range, or {@code null} if unspecified
+         * @see <a href="https://goo.gl/edrup4">Maven version range specification</a>
+         * @see <a href="https://goo.gl/WBsFIu">Maven version design document</a>
+         */
         @Nullable
         public String getVersion() {
             return this.version;
