344e320c992ddd5238ec2cbdbe01e8f923a4a9c9
Zach Levis <zml@aoeu.xyz>
2021-02-24 18:19:47 -0800

    build: Switch to using new SpongeGradle convention plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,7 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-
 plugins {
-    val indraVersion = "1.2.1"
-    checkstyle
-    id("net.kyori.indra") version indraVersion
-    id("net.kyori.indra.publishing.sonatype") version indraVersion
-    id("net.kyori.indra.license-header") version indraVersion
+    id("org.spongepowered.gradle.sponge.dev") version "1.0-SNAPSHOT"
+    id("net.kyori.indra.checkstyle") version "1.3.1"
+    id("net.kyori.indra.publishing.sonatype") version "1.3.1"
 }
 
 dependencies {
@@ -14,40 +10,25 @@ dependencies {
 }
 
 allprojects {
-    apply(plugin = "net.kyori.indra")
-    apply(plugin = "net.kyori.indra.license-header")
+    apply(plugin = "org.spongepowered.gradle.sponge.dev")
     apply(plugin = "net.kyori.indra.publishing.sonatype")
-    apply(plugin = "checkstyle")
+    apply(plugin = "net.kyori.indra.checkstyle")
 
     repositories {
-        maven("https://repo.spongepowered.org/repository/maven-public/")
+        org.spongepowered.gradle.convention.ConventionConstants.spongeRepo(this)
     }
 
-    val spongeSnapshotRepo = project.findProperty("spongeSnapshotRepo") as String?
-    val spongeReleaseRepo = project.findProperty("spongeReleaseRepo") as String?
-    indra {
-        github("SpongePowered", "plugin-meta") {
+    spongeConvention {
+        repository("plugin-meta") {
             ci = true
             publishing = true
         }
         mitLicense()
-
-        configurePublications {
-            pom {
-                developers {
-                    developer {
-                        name.set("SpongePowered Team")
-                        email.set("staff@spongepowered.org")
-                    }
-                }
-            }
-        }
-
-        if (spongeReleaseRepo != null && spongeSnapshotRepo != null) {
-            publishSnapshotsTo("sponge", spongeSnapshotRepo)
-            publishReleasesTo("sponge", spongeReleaseRepo)
+        licenseParameters {
+            this["organization"] = rootProject.property("organization")
         }
     }
+    indra.checkstyle.set("8.40")
 
     val sourceOutput by configurations.registering
     val main by sourceSets
@@ -61,73 +42,25 @@ allprojects {
         testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
     }
 
-    checkstyle {
-        toolVersion = "8.39"
-        configFile = rootProject.file("config/checkstyle/checkstyle.xml")
-    }
-
     tasks {
         jar {
-            manifest {
-                attributes(mapOf(
-                        "Git-Commit" to grgit.head().id,
-                        "Git-Branch" to grgit.branch.current().name,
-                        "Specification-Title" to "plugin-spi",
-                        "Specification-Vendor" to "SpongePowered",
-                        "Specification-Version" to archiveVersion.get(), // We are version 1 of ourselves
-                        "Created-By" to "${System.getProperty("java.version")} (${System.getProperty("java.vendor")})"
-                ))
-            }
+            manifest.attributes(mapOf(
+                // todo: drop grgit
+                "Git-Commit" to grgit.head().id,
+                "Git-Branch" to grgit.branch.current().name
+            ))
         }
 
         javadoc {
             options {
-                isFailOnError = false
                 (this as StandardJavadocDocletOptions).apply {
                     links(
                         "http://www.slf4j.org/apidocs/",
                         "https://google.github.io/guava/releases/21.0/api/docs/",
-                        "https://google.github.io/guice/api-docs/4.1/javadoc/",
-                        "http://asm.ow2.org/asm50/javadoc/user/"
+                        "https://google.github.io/guice/api-docs/4.1/javadoc/"
                     )
-
-                    if (JavaVersion.current() < JavaVersion.VERSION_12) {
-                        addBooleanOption("-no-module-directories", true)
-                    }
                 }
             }
-        }
-
-        test {
-            testLogging {
-                // Always print full stack trace if something goes wrong in the unit tests
-                exceptionFormat = TestExceptionFormat.FULL
-                showStandardStreams = true
-            }
-        }
-    }
-
-    license {
-        header = rootProject.file("HEADER.txt")
-        newLine = false
-
-        ext["name"] = rootProject.name
-        ext["organization"] = rootProject.property("organization")
-    }
-
-    // Signing, using specified private key file
-    signing {
-        val spongeSigningKey = project.findProperty("spongeSigningKey") as String?
-        val spongeSigningPassword = project.findProperty("spongeSigningPassword") as String?
-        if (spongeSigningKey != null && spongeSigningPassword != null) {
-            val keyFile = file(spongeSigningKey)
-            if (keyFile.exists()) {
-                useInMemoryPgpKeys(file(spongeSigningKey).readText(Charsets.UTF_8), spongeSigningPassword)
-            } else {
-                useInMemoryPgpKeys(spongeSigningKey, spongeSigningPassword)
-            }
-        } else {
-            signatories = PgpSignatoryProvider() // don't use gpg agent
         }
     }
 }
