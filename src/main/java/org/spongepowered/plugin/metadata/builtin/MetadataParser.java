3e73eaa76cbe9b1e925421b7707946c158c52528
Chris Sanders <zidane@spongepowered.org>
2021-08-31 22:25:17 -0500

    More MetadataParser improvements
    - Add read/write that take Reader/Writer (for flexibility)
    - Javadocs
    
    Signed-off-by: Chris Sanders <zidane@spongepowered.org>

--- src/main/java/org/spongepowered/plugin/metadata/builtin/MetadataParser.java
+++ src/main/java/org/spongepowered/plugin/metadata/builtin/MetadataParser.java
@@ -26,10 +26,12 @@ package org.spongepowered.plugin.metadata.builtin;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
@@ -45,15 +47,52 @@ public final class MetadataParser {
                 ;
     }
 
+    /**
+     * Reads a {@link MetadataContainer container} from a given {@link Path path} with configured {@link Gson deserializer}.
+     * <p>
+     * To get a standard deserializer, {@link MetadataParser#gsonBuilder()} is available.
+     * @param path The path
+     * @param gson The deserializer
+     * @return The container
+     * @throws IOException if the container fails to be read
+     */
     public static MetadataContainer read(final Path path, final Gson gson) throws IOException {
         Objects.requireNonNull(path, "path");
         Objects.requireNonNull(gson, "gson");
 
-        try (final JsonReader reader = new JsonReader(Files.newBufferedReader(path))) {
+        try (final BufferedReader reader = Files.newBufferedReader(path)) {
             return gson.fromJson(reader, MetadataContainer.class);
         }
     }
 
+    /**
+     * Reads a {@link MetadataContainer container} from a given {@link Reader reader} with configured {@link Gson deserializer}.
+     * <p>
+     * To get a standard deserializer, {@link MetadataParser#gsonBuilder()} is available.
+     * <p>
+     * The reader is not closed in this call therefore callers need to be sure to do so.
+     * @param reader The reader
+     * @param gson The deserializer
+     * @return The container
+     * @throws IOException if the container fails to deserialize
+     */
+    public static MetadataContainer read(final Reader reader, final Gson gson) throws IOException {
+        Objects.requireNonNull(reader, "reader");
+        Objects.requireNonNull(gson, "gson");
+
+        return gson.fromJson(reader, MetadataContainer.class);
+    }
+
+    /**
+     * Writes a {@link MetadataContainer container} to the given {@link Path path} using the configured {@link Gson serializer}.
+     * <p>
+     * To get a standard serializer, {@link MetadataParser#gsonBuilder()} is available.
+     * @param path The path
+     * @param container The container
+     * @param gson The serializer
+     * @param indent True to indent (pretty print) the resulting JSON, false if not
+     * @throws IOException If the container fails to serialize
+     */
     public static void write(final Path path, final MetadataContainer container, final Gson gson, final boolean indent) throws IOException {
         Objects.requireNonNull(path, "path");
         Objects.requireNonNull(container, "container");
@@ -64,6 +103,31 @@ public final class MetadataParser {
                 writer.setIndent("  ");
             }
             gson.toJson(container, MetadataContainer.class, writer);
+        }
+    }
+
+    /**
+     * Writes a {@link MetadataContainer container} to the given {@link Path path} using the configured {@link Gson serializer}.
+     * <p>
+     * To get a standard serializer, {@link MetadataParser#gsonBuilder()} is available.
+     * <p>
+     * The writer is not closed in this call therefore callers need to be sure to do so.
+     * @param writer The writer
+     * @param container The container
+     * @param gson The serializer
+     * @param indent True to indent (pretty print) the resulting JSON, false if not
+     * @throws IOException If the container fails to serialize
+     */
+    public static void write(final Writer writer, final MetadataContainer container, final Gson gson, final boolean indent) throws IOException {
+        Objects.requireNonNull(writer, "writer");
+        Objects.requireNonNull(container, "container");
+        Objects.requireNonNull(gson, "gson");
+
+        try (final JsonWriter jsonWriter = new JsonWriter(writer)) {
+            if (indent) {
+                jsonWriter.setIndent("  ");
+            }
+            gson.toJson(container, MetadataContainer.class, jsonWriter);
         }
     }
 
