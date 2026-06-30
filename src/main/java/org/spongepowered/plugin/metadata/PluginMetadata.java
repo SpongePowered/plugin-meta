AutoArchives/spongepowered-plugin-meta

Remove Container and Inheritable from API (not from builtin impl)
- Container has no use in plugin-spi.
- Inheritable can only have optional values but PluginMetadata has required values. In consequence, we had some null-like workaround values such as NullVersion.
- Third-parties are free to implement their own PluginMetadata, so forcing some values to be inheritable made no sense as it is a detail of the built-in impl.
- Loader and license metadata can now be specified per plugin.
- Global values and specific values used to be fully merged in StandardPluginMetadata with no ability to seperate them back during serialization.

src/main/java/org/spongepowered/plugin/metadata/PluginMetadata.java
@@ -24,20 +24,24 @@
  */
 package org.spongepowered.plugin.metadata;
 
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.spongepowered.plugin.metadata.model.PluginBranding;
+import org.spongepowered.plugin.metadata.model.PluginContributor;
+import org.spongepowered.plugin.metadata.model.PluginDependency;
+import org.spongepowered.plugin.metadata.model.PluginLinks;
+import org.spongepowered.plugin.metadata.model.PluginLoaderSpec;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
- * Represents specific, unique metadata to a plugin.
+ * Represents the metadata of a plugin.
  *
- * @see Inheritable Inheritable, for metadata that might be shared between multiple plugin metadata
  * @see org.spongepowered.plugin.metadata.builtin.StandardPluginMetadata StandardPluginMetadata, for a generic implementation
  */
-public interface PluginMetadata extends Inheritable {
-
-    /**
-     * @return The {@link Container container}.
-     */
-    Container container();
+public interface PluginMetadata {
 
     /**
      * Gets the {@link String id}.
@@ -65,6 +69,16 @@ public interface PluginMetadata extends Inheritable {
     String entrypoint();
 
     /**
+     * @return The {@link ArtifactVersion version}.
+     */
+    ArtifactVersion version();
+
+    /**
+     * @return The {@link PluginLoaderSpec loader}.
+     */
+    PluginLoaderSpec loader();
+
+    /**
      * @return The {@link String name} or {@link Optional#empty()} otherwise.
      */
     Optional<String> name();
@@ -73,4 +87,57 @@ public interface PluginMetadata extends Inheritable {
      * @return The {@link String description} or {@link Optional#empty()} otherwise.
      */
     Optional<String> description();
+
+    /**
+     * Gets the {@link String license} of the data within this container.
+     * <p>
+     * Notable examples include MIT or All Rights Reserved.
+     *
+     * @return The license
+     */
+    Optional<String> license();
+
+    /**
+     * @return The {@link PluginBranding branding}.
+     */
+    PluginBranding branding();
+
+    /**
+     * @return The {@link PluginLinks links} to various web resources.
+     */
+    PluginLinks links();
+
+    /**
+     * @return The {@link PluginContributor contributors} as an unmodifiable {@link List}.
+     */
+    List<PluginContributor> contributors();
+
+    /**
+     * Gets the {@link PluginDependency plugin dependency} by {@link String id}.
+     * <p>
+     * This maps to {@link PluginDependency#id()}.
+     * @param id The id
+     * @return The dependency or {@link Optional#empty()} otherwise.
+     */
+    Optional<PluginDependency> dependency(String id);
+
+    /**
+     * @return The {@link PluginDependency dependencies} as an unmodifiable {@link Collection}.
+     */
+    Collection<PluginDependency> dependencies();
+
+    /**
+     * Gets the {@link T property} by {@link String key}.
+     *
+     * @param key The key
+     * @param <T> The type
+     * @return The property or {@link Optional#empty()} otherwise.
+     */
+    <T> Optional<T> property(String key);
+
+    /**
+     * @return The properties as an unmodifiable {@link Map}.
+     */
+    Map<String, Object> properties();
+
 }

src/main/java/org/spongepowered/plugin/metadata/builtin/MetadataParser.java
@@ -30,8 +30,8 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.artifact.versioning.VersionRange;
+import org.spongepowered.plugin.metadata.builtin.adapter.InheritableMetadataAdapter;
 import org.spongepowered.plugin.metadata.builtin.adapter.MetadataContainerAdapter;
-import org.spongepowered.plugin.metadata.builtin.adapter.StandardInheritableAdapter;
 import org.spongepowered.plugin.metadata.builtin.adapter.StandardPluginMetadataBuilderDeserializer;
 import org.spongepowered.plugin.metadata.builtin.adapter.StandardPluginMetadataSerializer;
 import org.spongepowered.plugin.metadata.builtin.adapter.model.*;
@@ -52,15 +52,15 @@ import java.util.Set;
 
 public final class MetadataParser {
     private static final Gson GSON = new GsonBuilder()
-            .registerTypeAdapter(ContainerLoader.class, new ContainerLoaderAdapter())
+            .registerTypeAdapter(PluginLoaderSpec.class, new PluginLoaderSpecAdapter())
             .registerTypeAdapter(PluginBranding.class, new PluginBrandingAdapter())
             .registerTypeAdapter(PluginContributor.class, new PluginContributorAdapter())
             .registerTypeAdapter(PluginDependency.class, new PluginDependencyAdapter())
             .registerTypeAdapter(PluginLinks.class, new PluginLinksAdapter())
             .registerTypeAdapter(ArtifactVersion.class, new ArtifactVersionAdapter())
             .registerTypeAdapter(VersionRange.class, new VersionRangeAdapter())
+            .registerTypeAdapter(InheritableMetadata.class, new InheritableMetadataAdapter())
             .registerTypeAdapter(MetadataContainer.class, new MetadataContainerAdapter())
-            .registerTypeAdapter(StandardInheritable.class, new StandardInheritableAdapter())
             .registerTypeAdapter(StandardPluginMetadata.Builder.class, new StandardPluginMetadataBuilderDeserializer())
             .registerTypeAdapter(StandardPluginMetadata.class, new StandardPluginMetadataSerializer())
             .create();

src/main/java/org/spongepowered/plugin/metadata/builtin/adapter/InheritableMetadataAdapter.java
@@ -26,24 +26,28 @@ package org.spongepowered.plugin.metadata.builtin.adapter;
 
 import com.google.gson.*;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
-import org.spongepowered.plugin.metadata.builtin.NullVersion;
-import org.spongepowered.plugin.metadata.builtin.StandardInheritable;
+import org.spongepowered.plugin.metadata.builtin.InheritableMetadata;
+import org.spongepowered.plugin.metadata.builtin.adapter.util.GsonUtils;
 import org.spongepowered.plugin.metadata.model.PluginBranding;
 import org.spongepowered.plugin.metadata.model.PluginContributor;
 import org.spongepowered.plugin.metadata.model.PluginDependency;
 import org.spongepowered.plugin.metadata.model.PluginLinks;
-import org.spongepowered.plugin.metadata.builtin.adapter.util.GsonUtils;
+import org.spongepowered.plugin.metadata.model.PluginLoaderSpec;
 
 import java.lang.reflect.Type;
 import java.util.LinkedHashMap;
 
-public final class StandardInheritableAdapter implements JsonSerializer<StandardInheritable>, JsonDeserializer<StandardInheritable> {
+public final class InheritableMetadataAdapter implements JsonSerializer<InheritableMetadata>, JsonDeserializer<InheritableMetadata> {
 
     @Override
-    public StandardInheritable deserialize(final JsonElement element, final Type type, final JsonDeserializationContext context) throws JsonParseException {
+    public InheritableMetadata deserialize(final JsonElement element, final Type type, final JsonDeserializationContext context) throws JsonParseException {
         final JsonObject obj = element.getAsJsonObject();
-        return new StandardInheritable.Builder()
-                .version(GsonUtils.optional(obj, "version").map(JsonElement::getAsString).orElse(null))
+        return InheritableMetadata.builder()
+                .version(GsonUtils.optional(obj, "version").map(v -> context.<ArtifactVersion>deserialize(v, ArtifactVersion.class)).orElse(null))
+                .loader(GsonUtils.optional(obj, "loader").map(v -> context.<PluginLoaderSpec>deserialize(v, PluginLoaderSpec.class)).orElse(null))
+                .name(GsonUtils.optional(obj, "name").map(JsonElement::getAsString).orElse(null))
+                .description(GsonUtils.optional(obj, "description").map(JsonElement::getAsString).orElse(null))
+                .license(GsonUtils.optional(obj, "license").map(JsonElement::getAsString).orElse(null))
                 .branding(GsonUtils.optional(obj, "branding").map(v -> context.<PluginBranding>deserialize(v, PluginBranding.class)).orElseGet(PluginBranding::none))
                 .links(GsonUtils.optional(obj, "links").map(v -> context.<PluginLinks>deserialize(v, PluginLinks.class)).orElseGet(PluginLinks::none))
                 .contributors(GsonUtils.stream(obj, "contributors").map(v -> context.<PluginContributor>deserialize(v, PluginContributor.class)).toList())
@@ -53,22 +57,24 @@ public final class StandardInheritableAdapter implements JsonSerializer<Standard
     }
 
     @Override
-    public JsonElement serialize(final StandardInheritable value, final Type type, final JsonSerializationContext context) {
+    public JsonElement serialize(final InheritableMetadata value, final Type type, final JsonSerializationContext context) {
         final JsonObject obj = new JsonObject();
-        if (value.version() != NullVersion.instance()) {
-            obj.add("version", context.serialize(value.version(), ArtifactVersion.class));
-        }
-        if (value.branding() != PluginBranding.none()) {
+        value.version().ifPresent(v -> obj.add("version", context.serialize(v, ArtifactVersion.class)));
+        value.loader().ifPresent(v -> obj.add("loader", context.serialize(v, PluginLoaderSpec.class)));
+        value.name().ifPresent(v -> obj.addProperty("name", v));
+        value.description().ifPresent(v -> obj.addProperty("description", v));
+        value.license().ifPresent(v -> obj.addProperty("license", v));
+        if (!value.branding().equals(PluginBranding.none())) {
             obj.add("branding", context.serialize(value.branding(), PluginBranding.class));
         }
-        if (value.links() != PluginLinks.none()) {
+        if (!value.links().equals(PluginLinks.none())) {
             obj.add("links", context.serialize(value.links(), PluginLinks.class));
         }
         if (!value.contributors().isEmpty()) {
             obj.add("contributors", GsonUtils.toArray(value.contributors().stream().map(v -> context.serialize(v, PluginContributor.class))));
         }
         if (!value.dependencies().isEmpty()) {
-            obj.add("dependencies", GsonUtils.toArray(value.dependencies().stream().map(v -> context.serialize(v, PluginDependency.class))));
+            obj.add("dependencies", GsonUtils.toArray(value.dependencies().values().stream().map(v -> context.serialize(v, PluginDependency.class))));
         }
         if (!value.properties().isEmpty()) {
             obj.add("properties", GsonUtils.serializeMap(value.properties(), v -> new JsonPrimitive(v.toString())));

SpongePowered/plugin-meta

src/main/java/org/spongepowered/plugin/metadata/PluginMetadata.java
@@ -24,20 +24,24 @@
  */
 package org.spongepowered.plugin.metadata;
 
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+import org.spongepowered.plugin.metadata.model.PluginBranding;
+import org.spongepowered.plugin.metadata.model.PluginContributor;
+import org.spongepowered.plugin.metadata.model.PluginDependency;
+import org.spongepowered.plugin.metadata.model.PluginLinks;
+import org.spongepowered.plugin.metadata.model.PluginLoaderSpecification;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 /**
- * Represents specific, unique metadata to a plugin.
+ * Represents the metadata of a plugin.
  *
- * @see Inheritable Inheritable, for metadata that might be shared between multiple plugin metadata
  * @see org.spongepowered.plugin.metadata.builtin.StandardPluginMetadata StandardPluginMetadata, for a generic implementation
  */
-public interface PluginMetadata extends Inheritable {
-
-    /**
-     * @return The {@link Container container}.
-     */
-    Container container();
+public interface PluginMetadata {
 
     /**
      * Gets the {@link String id}.
@@ -65,6 +69,16 @@ public interface PluginMetadata extends Inheritable {
     String entrypoint();
 
     /**
+     * @return The {@link ArtifactVersion version}.
+     */
+    ArtifactVersion version();
+
+    /**
+     * @return The {@link PluginLoaderSpecification loader}.
+     */
+    PluginLoaderSpecification loader();
+
+    /**
      * @return The {@link String name} or {@link Optional#empty()} otherwise.
      */
     Optional<String> name();
@@ -73,4 +87,57 @@ public interface PluginMetadata extends Inheritable {
      * @return The {@link String description} or {@link Optional#empty()} otherwise.
      */
     Optional<String> description();
+
+    /**
+     * Gets the {@link String license} of the data within this container.
+     * <p>
+     * Notable examples include MIT or All Rights Reserved.
+     *
+     * @return The license
+     */
+    Optional<String> license();
+
+    /**
+     * @return The {@link PluginBranding branding}.
+     */
+    PluginBranding branding();
+
+    /**
+     * @return The {@link PluginLinks links} to various web resources.
+     */
+    PluginLinks links();
+
+    /**
+     * @return The {@link PluginContributor contributors} as an unmodifiable {@link List}.
+     */
+    List<PluginContributor> contributors();
+
+    /**
+     * Gets the {@link PluginDependency plugin dependency} by {@link String id}.
+     * <p>
+     * This maps to {@link PluginDependency#id()}.
+     * @param id The id
+     * @return The dependency or {@link Optional#empty()} otherwise.
+     */
+    Optional<PluginDependency> dependency(String id);
+
+    /**
+     * @return The {@link PluginDependency dependencies} as an unmodifiable {@link Collection}.
+     */
+    Collection<PluginDependency> dependencies();
+
+    /**
+     * Gets the {@link T property} by {@link String key}.
+     *
+     * @param key The key
+     * @param <T> The type
+     * @return The property or {@link Optional#empty()} otherwise.
+     */
+    <T> Optional<T> property(String key);
+
+    /**
+     * @return The properties as an unmodifiable {@link Map}.
+     */
+    Map<String, Object> properties();
+
 }

src/main/java/org/spongepowered/plugin/metadata/builtin/MetadataParser.java
@@ -30,8 +30,8 @@ import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
 import org.apache.maven.artifact.versioning.VersionRange;
+import org.spongepowered.plugin.metadata.builtin.adapter.InheritableMetadataAdapter;
 import org.spongepowered.plugin.metadata.builtin.adapter.MetadataContainerAdapter;
-import org.spongepowered.plugin.metadata.builtin.adapter.StandardInheritableAdapter;
 import org.spongepowered.plugin.metadata.builtin.adapter.StandardPluginMetadataBuilderDeserializer;
 import org.spongepowered.plugin.metadata.builtin.adapter.StandardPluginMetadataSerializer;
 import org.spongepowered.plugin.metadata.builtin.adapter.model.*;
@@ -52,15 +52,15 @@ import java.util.Set;
 
 public final class MetadataParser {
     private static final Gson GSON = new GsonBuilder()
-            .registerTypeAdapter(ContainerLoader.class, new ContainerLoaderAdapter())
+            .registerTypeAdapter(PluginLoaderSpecification.class, new PluginLoaderSpecificationAdapter())
             .registerTypeAdapter(PluginBranding.class, new PluginBrandingAdapter())
             .registerTypeAdapter(PluginContributor.class, new PluginContributorAdapter())
             .registerTypeAdapter(PluginDependency.class, new PluginDependencyAdapter())
             .registerTypeAdapter(PluginLinks.class, new PluginLinksAdapter())
             .registerTypeAdapter(ArtifactVersion.class, new ArtifactVersionAdapter())
             .registerTypeAdapter(VersionRange.class, new VersionRangeAdapter())
+            .registerTypeAdapter(InheritableMetadata.class, new InheritableMetadataAdapter())
             .registerTypeAdapter(MetadataContainer.class, new MetadataContainerAdapter())
-            .registerTypeAdapter(StandardInheritable.class, new StandardInheritableAdapter())
             .registerTypeAdapter(StandardPluginMetadata.Builder.class, new StandardPluginMetadataBuilderDeserializer())
             .registerTypeAdapter(StandardPluginMetadata.class, new StandardPluginMetadataSerializer())
             .create();

src/main/java/org/spongepowered/plugin/metadata/builtin/adapter/InheritableMetadataAdapter.java
@@ -26,24 +26,28 @@ package org.spongepowered.plugin.metadata.builtin.adapter;
 
 import com.google.gson.*;
 import org.apache.maven.artifact.versioning.ArtifactVersion;
-import org.spongepowered.plugin.metadata.builtin.NullVersion;
-import org.spongepowered.plugin.metadata.builtin.StandardInheritable;
+import org.spongepowered.plugin.metadata.builtin.InheritableMetadata;
+import org.spongepowered.plugin.metadata.builtin.adapter.util.GsonUtils;
 import org.spongepowered.plugin.metadata.model.PluginBranding;
 import org.spongepowered.plugin.metadata.model.PluginContributor;
 import org.spongepowered.plugin.metadata.model.PluginDependency;
 import org.spongepowered.plugin.metadata.model.PluginLinks;
-import org.spongepowered.plugin.metadata.builtin.adapter.util.GsonUtils;
+import org.spongepowered.plugin.metadata.model.PluginLoaderSpecification;
 
 import java.lang.reflect.Type;
 import java.util.LinkedHashMap;
 
-public final class StandardInheritableAdapter implements JsonSerializer<StandardInheritable>, JsonDeserializer<StandardInheritable> {
+public final class InheritableMetadataAdapter implements JsonSerializer<InheritableMetadata>, JsonDeserializer<InheritableMetadata> {
 
     @Override
-    public StandardInheritable deserialize(final JsonElement element, final Type type, final JsonDeserializationContext context) throws JsonParseException {
+    public InheritableMetadata deserialize(final JsonElement element, final Type type, final JsonDeserializationContext context) throws JsonParseException {
         final JsonObject obj = element.getAsJsonObject();
-        return new StandardInheritable.Builder()
-                .version(GsonUtils.optional(obj, "version").map(JsonElement::getAsString).orElse(null))
+        return InheritableMetadata.builder()
+                .version(GsonUtils.optional(obj, "version").map(v -> context.<ArtifactVersion>deserialize(v, ArtifactVersion.class)).orElse(null))
+                .loader(GsonUtils.optional(obj, "loader").map(v -> context.<PluginLoaderSpecification>deserialize(v, PluginLoaderSpecification.class)).orElse(null))
+                .name(GsonUtils.optional(obj, "name").map(JsonElement::getAsString).orElse(null))
+                .description(GsonUtils.optional(obj, "description").map(JsonElement::getAsString).orElse(null))
+                .license(GsonUtils.optional(obj, "license").map(JsonElement::getAsString).orElse(null))
                 .branding(GsonUtils.optional(obj, "branding").map(v -> context.<PluginBranding>deserialize(v, PluginBranding.class)).orElseGet(PluginBranding::none))
                 .links(GsonUtils.optional(obj, "links").map(v -> context.<PluginLinks>deserialize(v, PluginLinks.class)).orElseGet(PluginLinks::none))
                 .contributors(GsonUtils.stream(obj, "contributors").map(v -> context.<PluginContributor>deserialize(v, PluginContributor.class)).toList())
@@ -53,22 +57,24 @@ public final class StandardInheritableAdapter implements JsonSerializer<Standard
     }
 
     @Override
-    public JsonElement serialize(final StandardInheritable value, final Type type, final JsonSerializationContext context) {
+    public JsonElement serialize(final InheritableMetadata value, final Type type, final JsonSerializationContext context) {
         final JsonObject obj = new JsonObject();
-        if (value.version() != NullVersion.instance()) {
-            obj.add("version", context.serialize(value.version(), ArtifactVersion.class));
-        }
-        if (value.branding() != PluginBranding.none()) {
+        value.version().ifPresent(v -> obj.add("version", context.serialize(v, ArtifactVersion.class)));
+        value.loader().ifPresent(v -> obj.add("loader", context.serialize(v, PluginLoaderSpecification.class)));
+        value.name().ifPresent(v -> obj.addProperty("name", v));
+        value.description().ifPresent(v -> obj.addProperty("description", v));
+        value.license().ifPresent(v -> obj.addProperty("license", v));
+        if (!value.branding().equals(PluginBranding.none())) {
             obj.add("branding", context.serialize(value.branding(), PluginBranding.class));
         }
-        if (value.links() != PluginLinks.none()) {
+        if (!value.links().equals(PluginLinks.none())) {
             obj.add("links", context.serialize(value.links(), PluginLinks.class));
         }
         if (!value.contributors().isEmpty()) {
             obj.add("contributors", GsonUtils.toArray(value.contributors().stream().map(v -> context.serialize(v, PluginContributor.class))));
         }
         if (!value.dependencies().isEmpty()) {
-            obj.add("dependencies", GsonUtils.toArray(value.dependencies().stream().map(v -> context.serialize(v, PluginDependency.class))));
+            obj.add("dependencies", GsonUtils.toArray(value.dependencies().values().stream().map(v -> context.serialize(v, PluginDependency.class))));
         }
         if (!value.properties().isEmpty()) {
             obj.add("properties", GsonUtils.serializeMap(value.properties(), v -> new JsonPrimitive(v.toString())));